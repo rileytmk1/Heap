01e52b46123544bac33287aa7529a96ee55f465f
U-BSD\459535 <459535@SHS-95W2NW3.beaverton.k12.or.us>
2025-02-10 10:11:04 -0800

    save

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void add();
+void print();
+
+
+
+int main()
+{
+  int *a = new int[100];
+  char input [10];
+  cout << "Manual input (m) or file (f)?" << endl;
+  while (strcmp(input, "QUIT") != 0){
+    if (strcmp(input, "f") == 0){
+      vector <int> f;
+      int n;
+      while(
+    }
+  }
+  
+}
+
+void add (int* &a, 
+
+
+
+
+
+
+
+  
