1f6e526629e146374f29a5d5044592a5fbb6ea47
Tom Thammakhoune <459535@bsd48.org>
2025-02-12 06:23:26 +0000

    save

--- main.cpp
+++ main.cpp
@@ -2,31 +2,71 @@
 #include <cstring>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-void add();
-void print();
-
+void insert(int n, int* &heap, int &c);
+void print(int* &heap, int &c);
+void remove_root(int* &heap);
 
 
 int main()
 {
-  int *a = new int[100];
+  int *heap = new int[100];
   char input [10];
-  cout << "Manual input (m) or file (f)?" << endl;
+  int c = 0;
+  cout << "ADD, DELETE, or PRINT" << endl;
   while (strcmp(input, "QUIT") != 0){
-    if (strcmp(input, "f") == 0){
-      vector <int> f;
-      int n;
-      while(
+    cin.get(input, 10);
+    cin.get();
+    if (strcmp(input, "ADD") == 0){
+      cout << "manual(m) or file(f)? ";
+      char input2[10];
+      cin.get(input2, 10);
+      cin.get();
+      if (strcmp(input2, "m") == 0){
+	int n;
+	cout << "What number do you want to add? " << endl;
+	cin >> n;
+	cin.ignore();
+	insert(n, heap, c);
+      }
+            
+    }
+    else if (strcmp(input, "print") == 0) {
+      print(heap, c);
     }
   }
   
 }
 
-void add (int* &a, 
+void insert (int n, int* & heap, int &c)
+{
+  heap[c] = n;
+  int i = c;
+  while (i != 0){
+    int parent = floor((i-1)/2);
+    if (heap[parent] < heap[i]){
+      int temp = heap[parent];
+      heap[parent] = heap[i];
+      heap[i] = temp;
+      i = parent;
+    }
+    else{
+      break;
+    }
+  }
+  c++;
+}
 
+void print(int* &heap, int &c)
+{
+    for (int i = 0; i < c; ++i)
+        cout << heap[i] << " ";
+
+    cout << "\n";
+}
 
 
 
